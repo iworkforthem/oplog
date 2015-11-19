e44f131b4c869a5bf8f4d81e353f4241f359191e
Wai Mun <iworkforthem@gmail.com>
2015-11-19 22:31:08 +0800

    Get the highest timestamp in oplog
    Get the highest timestamp in the oplog, limit to only 1.

--- service.js
+++ service.js
@@ -6,10 +6,31 @@ MongoClient.connect(url, function(err, db) {
   console.log("Connected correctly to: " + url);
 
   db.collection('oplog.$main', function (err, col) {      // oplog.rs for Replica Set | oplog.$main for Master/Slave Replication
+      var lastOplogTime, queryForTime;
+
+      col.find({}, {
+        ts: 1
+      }).sort({
+        $natural: -1
+      }).limit(1).toArray(function(err, data) {
+        lastOplogTime = data[0].ts;
+        // If there isn't one found, get one from the local clock
+        if (lastOplogTime) {
+          queryForTime = {
+            $gt: lastOplogTime
+          };
+        } else {
+          tstamp = new MongoDB.Timestamp(0, Math.floor(new Date().getTime() / 1000))
+          queryForTime = {
+            $gt: tstamp
+          };
+        }
+      });
 
       var stream = col.find({
         ns: 'll_staging.statements',                      // todo: change to my collection
-        op: 'i'                                           // i for insert | u for update | d for delete
+        op: 'i',                                          // i for insert | u for update | d for delete
+        ts: queryForTime
       },{
         tailable: true,
         awaitData: true,
